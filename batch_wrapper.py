3485d0dee415a40788adba21ac2269f28e9bedbf
Peter Melchior <peter.m.melchior@gmail.com>
2022-08-01 23:57:59 -0400

    rewrote with BatchedFilesDataset; added Instrument

--- batch_wrapper.py
+++ batch_wrapper.py
@@ -1,31 +1,105 @@
 #!/usr/bin/env python
 # coding: utf-8
-import os,sys,time
-import numpy as np 
+import os, glob, sys, time, io
+import numpy as np
 import astropy.io.fits as fits
 import multiprocessing as mp
-import pickle,torch
-import humanize,psutil
-from util import get_norm
-from line_profiler import LineProfiler
-from memory_profiler import profile, memory_usage
+import pickle, random
+import torch
+from torch.utils.data import IterableDataset, DataLoader
+from itertools import cycle, chain
 
-def mem_report():
-    print("CPU RAM Free: " + humanize.naturalsize( psutil.virtual_memory().available ))
-    return
+from util import get_norm, mem_report
 
-def read_sdss_spectra(plate, mjd, fiberid): 
+
+############ Functions for creating batched files ###############
+
+class CPU_Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else: return super().find_class(module, name)
+
+def load_batch(batch_name):
+    with open(batch_name, 'rb') as f:
+        if torch.cuda.is_available():
+            batch_copy = pickle.load(f)
+        else:
+            batch_copy = CPU_Unpickler(f).load()
+
+    if type(batch_copy)==list:
+        spec,w,z = batch_copy[:3]
+        w[w<=1e-6] = 0
+        return spec,w,z
+    else:
+        return batch_copy
+
+
+# based on https://medium.com/speechmatics/how-to-build-a-streaming-dataloader-with-pytorch-a66dd891d9dd
+class BatchedFilesDataset(IterableDataset):
+
+    def __init__(self, file_list, shuffle=False, repeat=False):
+        assert len(file_list), "File list cannot be empty"
+        self.file_list = file_list
+        self.shuffle = shuffle
+        self.repeat = repeat
+
+    def process_data(self, idx):
+        if self.shuffle:
+            idx = random.randint(0, len(self.file_list) -1)
+        batch_name = self.file_list[idx]
+        data = load_batch(batch_name)
+        for x in zip(*data):
+            yield x
+
+    def get_stream(self):
+        if self.repeat:
+            return chain.from_iterable(map(self.process_data, cycle(range(len(self.file_list)))))
+        return chain.from_iterable(map(self.process_data, range(len(self.file_list))))
+
+    def __iter__(self):
+        return self.get_stream()
+
+    def __len__(self):
+        return len(self.file_list)
+
+def collect_batches(dir, name, tag="chunk1024", which=None):
+
+    assert name in ["SDSS", "BOSS"]
+    filename = f"{name}{tag}_*.pkl"
+    batch_files = glob.glob(dir + "/" + filename)
+    batches = [item for item in batch_files if not "copy" in item]
+
+    NBATCH = len(batches)
+    train_batches = batches[:int(0.7*NBATCH)]
+    valid_batches = batches[int(0.7*NBATCH):int(0.85*NBATCH)]
+    test_batches = batches[int(0.85*NBATCH):]
+
+    if which == "test": return test_batches
+    elif which == "valid": return valid_batches
+    elif which == "train": return train_batches
+    else: return all_batches
+
+def get_data_loader(dir, name, tag="chunk1024", which=None, batch_size=1024, shuffle=False, repeat=False):
+    files = collect_batches(dir, name, tag=tag, which=which)
+    # load data on demand, random order
+    data = BatchedFilesDataset(files, shuffle=shuffle, repeat=repeat)
+    return DataLoader(data, batch_size=batch_size)
+
+
+############ Functions for creating batched files ###############
+def read_sdss_spectra(plate, mjd, fiberid):
     flocal = os.path.join(data_dir, 'sdss-spectra/spec-%s-%i-%s.fits' % (str(plate).zfill(4), mjd, str(fiberid).zfill(4)))
-    if not os.path.isfile(flocal): 
+    if not os.path.isfile(flocal):
         flocal = os.path.join(data_dir, 'lite/%s/spec-%s-%i-%s.fits' % (str(plate).zfill(4),str(plate).zfill(4), mjd, str(fiberid).zfill(4)))
         if not os.path.isfile(flocal):raise ValueError
     hdulist = fits.open(flocal)
-    
+
     header = hdulist[0].header
     data = hdulist[1].data
     logw = data['loglam']
     spec = data['flux']
-    ivar = data['ivar'] 
+    ivar = data['ivar']
     mask = data['and_mask'].astype(bool)
     ivar[mask] = 0.
     return logw, spec, ivar, mask
@@ -46,32 +120,32 @@ def prepare_batch(input_list):
     zreds = np.zeros(Nspec, dtype=np.float32)
     zerrs = np.zeros(Nspec, dtype=np.float32)
     specid = []
-    
+
     ta = time.time()
-    
+
     interval = Nspec//5
 
     i = 0
     for j in range(len(targets[0])):
         plate, mjd, fiberid = [targets[k][j] for k in [0,1,2]]
-        try: 
+        try:
             x = read_sdss_spectra(plate, mjd, fiberid)
-        except:# (IndexError, OSError): 
+        except:# (IndexError, OSError):
             print("Error!",plate, mjd, fiberid)
-            continue 
+            continue
 
         Z = targets[3][j]
         Z_ERR = targets[4][j]
-        
+
         #print("i:",j,"j:",j,plate, mjd, fiberid, Z, Z_ERR)
         if np.sum(x[1]) == 0:continue
         if Z <= 0:continue
         if Z > 0.5:continue
         if np.max(np.abs(x[1]))>1e5:continue
-        
+
         iw = int(np.around((x[0][0] - wave[0])/0.0001))
 
-        if iw < 0: 
+        if iw < 0:
             print("x[0][0]:",x[0][0])
             continue
 
@@ -84,16 +158,16 @@ def prepare_batch(input_list):
 
         norm = get_norm(np.expand_dims(x[1], axis=0))
         w = x[2] * ~x[3] * (norm**2)[:,None]
-        
-        if norm<0: 
+
+        if norm<0:
             print("negative norm, continue...")
             continue
-        
+
         if (w<=0).all():
             print("All zero weight, continue...",plate, mjd, fiberid)
             continue
 
-        w[w<1e-6] = 1e-6  
+        w[w<1e-6] = 1e-6
         specs[i,iw:endpoint] = x[1][:endpoint-iw]/norm
         weights[i,iw:endpoint] = w[0][:endpoint-iw]
         zreds[i] = Z
@@ -102,7 +176,7 @@ def prepare_batch(input_list):
         specid.append("%d-%d-%d"%(plate,mjd,fiberid))
 
         i += 1
-        
+
         # collected all samples
         if i==Nspec:break
         if debug and (i % interval) == 0:
@@ -113,7 +187,7 @@ def prepare_batch(input_list):
             print("\n\n%d: remaining time = %.2f"%(i,remain_seg*t_seg))
             ta = tb
             if debug:mem_report()
-    
+
     if i<0.9*Nspec:
         print("filling fraction too low... %.2f"%(i/Nspec))
         return
@@ -121,14 +195,14 @@ def prepare_batch(input_list):
     rand_int = np.arange(len(specs))
     print("len(specs):",len(specs),#"specid:",specid,
           "rand_int",rand_int.max())
-    
+
     specid = np.array(specid)
-    batch_copy = [torch.tensor(specs[rand_int],device=device), 
+    batch_copy = [torch.tensor(specs[rand_int],device=device),
                   torch.tensor(weights[rand_int],device=device),
-                  torch.tensor(zreds[rand_int],device=device), 
+                  torch.tensor(zreds[rand_int],device=device),
                   specid[rand_int],norms[rand_int],
                   torch.tensor(zerrs[rand_int],device=device)]
-    
+
     batch_name = "%s%s_%d.pkl"%("",code,wh_start)
     save_batch(batch_copy,batch_name)
     return
@@ -137,14 +211,14 @@ def wrap_batches(which,tag,k_range=[0,10],Nspec=1024):
     header_dir = "/scratch/gpfs/yanliang/headers"
     headers = {0:"truncated-specobj.pkl",
                1:"boss_headers.pkl"}
-    
+
     header_name = headers[which]
     if "joint" in tag:header_name = "joint_headers.pkl"
-        
+
     f = open("%s/%s"%(header_dir,header_name),"rb")
     targets = pickle.load(f)
     f.close()
-    
+
     # all targets
     name = ["SDSS","BOSS"]
     id_names = ['PLATE','MJD','FIBERID',"Z","Z_ERR"]
@@ -152,24 +226,24 @@ def wrap_batches(which,tag,k_range=[0,10],Nspec=1024):
     if "joint" in header_name:
         keys = ["%s_%s"%(k,name[which]) for k in id_names]
     else: keys = id_names
-    
+
     #targets_slice = {}
     #for k in keys:targets_slice[k] = targets[k]
     targets_slice = []
     for k in keys:targets_slice.append(targets[k].data)
-    
+
     lower,upper = LOGWAVE_RANGE[which]
-    wave = np.arange(lower,upper, 0.0001).astype(np.float32) 
+    wave = np.arange(lower,upper, 0.0001).astype(np.float32)
 
     start,end = k_range[0]*Nspec,k_range[1]*Nspec
     start_index = np.arange(start,end, Nspec)
     nbatch = len(start_index)
-    
+
     size = len(targets_slice[0])
     print("nbatch:",nbatch,"start_index:",start_index)
-    
+
     code = "%s%s"%(name[which],tag)
-    
+
     input_list = []
     for k,index in enumerate(start_index):
         loc = range(index,min(index+3*Nspec,size))
@@ -178,7 +252,7 @@ def wrap_batches(which,tag,k_range=[0,10],Nspec=1024):
         args = [batch_targets,wave,code,index,Nspec]
         input_list.append(args)
         #print("prepared batch %d/%d, time=%.2f"%(k,nbatch,tb-ta))
-    
+
     n_pool = min(25,len(input_list))
     print("%d processes..."%(n_pool))
     pool = mp.Pool(n_pool)
@@ -187,20 +261,10 @@ def wrap_batches(which,tag,k_range=[0,10],Nspec=1024):
     pool.join()
     return
 
-debug = False
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-data_dir = '/scratch/gpfs/yanliang'
-dynamic_dir = "/scratch/gpfs/yanliang/dynamic-data"
-LOGWAVE_RANGE = [[3.578, 3.97],[3.549, 4.0175]]
 
-if "batch_wrapper" in sys.argv[0]:
-    which = int(sys.argv[1])
-    tag = sys.argv[2]
-    wrap_batches(which,tag,k_range=[0,20],Nspec=256)
-    """
-    lprofiler = LineProfiler()
-    lprofiler.add_function(read_sdss_spectra)
-    lp_wrapper = lprofiler(prepare_batch)
-    lp_wrapper()
-    lprofiler.print_stats()
-    """
+if __name__ == "__main__":
+
+    if "batch_wrapper" in sys.argv[0]:
+        which = int(sys.argv[1])
+        tag = sys.argv[2]
+        wrap_batches(which,tag,k_range=[0,20],Nspec=256)
--- util.py
+++ util.py
@@ -1,8 +1,9 @@
 import numpy as np
 import torch
 import torch.nn.functional as F
+import humanize, psutil, GPUtil
 from torchinterp1d import Interp1d
-
+from torchinterp1d import Interp1d
 
 def get_norm(x):
     # simple median as robust mean across the spectrum
@@ -56,92 +57,49 @@ def soft_weighted_medoid(x, temperature=1.0):
     return (s.unsqueeze(-1) * x).sum(dim=-2)
 
 
-# read in SDSS theta and spectra
-def load_data(path, which=None, device=None):
-
-    assert which in [None, "train", "valid", "test"]
-
-    data = np.load(path)
-
-    # define subsets
-    N = len(data['spectra'])
-    n_train, n_valid = int(N*0.7), int(N*0.15)
-    n_test = N - n_train - n_valid
-
-    sl = {None: slice(None),
-          "train": slice(0, n_train),
-          "valid": slice(n_train, n_train+n_valid),
-          "test": slice(n_train+n_valid, n_train+n_valid+n_test),
-         }
-    sl = sl[which]
-
-    y = data['spectra'][sl]
-    wave = 10**data['wave']
-    z = data['z'][sl]
-    zerr = np.maximum(data['zerr'][sl], 1e-6) # one case has zerr=0, but looks otherwise OK
-    ivar = data['ivar'][sl]
-    mask = data['mask'][sl]
-
-    # SDSS IDs
-    plates, mjds, fibers = data['plate'][sl], data['mjd'][sl], data['fiber'][sl]
-    id = [f"{plate}-{mjd}-{fiber}" for plate, mjd, fiber in zip(plates, mjds, fibers)]
-
-    # get normalization
-    norm = get_norm(y)
-
-    w = ivar * ~mask * (norm**2)[:,None]
-    sel = np.any(w > 0, axis=1)   # remove all spectra that have all zero weights
-    sel &= (norm > 0) & (z < 0.5)   # plus noisy ones and redshift outliers
-
-    w = np.maximum(w, 1e-6)       # avoid zero weights for logL normalization
-    w = w[sel]
-    y = y[sel] / norm[sel, None]
-    z = z[sel]
-    zerr = zerr[sel]
-    norm = norm[sel]
-    id = np.array(id)[sel]
-
-
-    print (f"Loading {len(y)} spectra (which = {which})")
-
-    y = torch.tensor(y, dtype=torch.float32, device=device)
-    w = torch.tensor(w, dtype=torch.float32, device=device)
-    z = torch.tensor(z, dtype=torch.float32, device=device)
-    zerr = torch.tensor(zerr, dtype=torch.float32, device=device)
-
-    return {"wave": wave,
-            "y": y,
-            "w": w,
-            "z": z,
-            "zerr": zerr,
-            "norm": norm,
-            "id": id,
-            "N": len(y),
-           }
-
-def load_model(fileroot):
+def load_model(fileroot, n_latent=10):
     if not torch.cuda.is_available():
         device = torch.device('cpu')
     else:
         device = None
-    
+
     path = f'{fileroot}.pt'
     model = torch.load(path, map_location=device)
+
     if type(model)==list or type(model)==tuple:
         [m.eval() for m in model]
+    elif type(model)==dict:
+        mdict = model
+        print("states:",mdict.keys())
+
+        models = mdict["model"]
+        instruments = mdict["instrument"]
+        model = []
+        if "n_latent" in mdict:n_latent=mdict["n_latent"]
+        for m in models:
+            loadm = SpectrumAutoencoder(wave_rest,n_latent=n_latent,
+                                        normalize=option_normalize)
+            loadm.load_state_dict(m)
+            loadm.eval()
+            model.append(loadm)
+
+        for ins in instruments:
+            empty=Instrument(wave_obs=None, calibration=None)
+            model.append(empty)
     else: model.eval()
+
     path = f'{fileroot}.losses.npy'
     loss = np.load(path)
     print (f"model {fileroot}: iterations {len(loss)}, final loss: {loss[-1]}")
     return model, loss
 
-def load_models(label, n_config):
+def load_models(label, n_config, n_latent=10):
     models, losses = {}, {}
     best_model, best_loss = 0, np.inf
     for i in range(n_config):
         try:
             label_ = label + f".{i}"
-            model, loss = load_model(label_)
+            model, loss = load_model(label_, n_latent=n_latent)
             models[i] = model
             losses[i] = loss
             if loss[-1][1] < best_loss:
@@ -152,49 +110,19 @@ def load_models(label, n_config):
 
     return models, losses, best_model
 
-def augment_batch(batch, how="mask", wave_obs=None):
-    assert how in ["mask", "redshift"]
 
-    spec, w, z = batch
-    N, L = spec.shape
+def skylines_mask(waves, intensity_limit=2, radii=5, debug=True):
 
-    if how == "mask":
-        # randomly mask ~1000 bins
-        delta = np.random.randint(500, 1500, N)
-        start = np.random.randint(0, L-delta)
-        end = start + delta
-        new_spec = spec.clone()
-        new_w = w.clone()
-        for i in range(N):
-            new_spec[i,start[i]:end[i]] = 0
-            new_w[i,start[i]:end[i]] = 0
-        return new_spec, new_w, z
-
-    if how == "redshift":
-        assert wave_obs is not None
-        z_max = 0.5
-        z_new = torch.rand(N).to(z.device) * z_max
-
-        # apply inverse of redshift correction
-        zfactor = ((1+z_new)/(1+z)).unsqueeze(1)
-
-        # redshift linear interpolation
-        spec_new = Interp1d()(wave_obs*zfactor, spec, wave_obs)
-        w_new = Interp1d()(wave_obs*zfactor, w, wave_obs)
-        return spec_new, w_new, z_new
-
-def skylines_mask(waves,intensity_limit=2,radii=5,debug=True):
-    
     f=open("sky-lines.txt","r")
     content = f.readlines()
     f.close()
-    
+
     skylines = [[10*float(line.split()[0]),float(line.split()[1])] for line in content if not line[0]=="#" ]
     skylines = np.array(skylines)
-    
+
     n_lines = 0
     mask = ~(waves>0)
-    
+
     for line in skylines:
         line_pos, intensity = line
         if line_pos>waves.max():continue
@@ -205,3 +133,120 @@ def skylines_mask(waves,intensity_limit=2,radii=5,debug=True):
     non_zero = torch.count_nonzero(mask)
     if debug:print("number of lines: %d, fraction: %.2f"%(n_lines,non_zero/mask.shape[0]))
     return mask
+
+def mem_report():
+    print("CPU RAM Free: " + humanize.naturalsize( psutil.virtual_memory().available ))
+
+    if torch.cuda.device_count() ==0: return
+
+    GPUs = GPUtil.getGPUs()
+    for i, gpu in enumerate(GPUs):
+        print('GPU {:d} ... Mem Free: {:.0f}MB / {:.0f}MB | Utilization {:3.0f}%'.format(i, gpu.memoryFree, gpu.memoryTotal, gpu.memoryUtil*100))
+    return
+
+
+class LogLinearDistribution():
+    def __init__(self, a, bound):
+        x0,xf = bound
+        self.bound = bound
+        self.a = a
+        self.norm = -a*np.log(10)/(10**(a*x0)-10**(a*xf))
+
+    def pdf(self,x):
+        pdf = self.norm*10**(self.a*x)
+        pdf[(x<self.bound[0])|(x>self.bound[1])] = 0
+        return pdf
+
+    def cdf(self,x):
+        factor = self.norm/(-self.a*np.log(10))
+        cdf = -factor*(10**(self.a*x)-10**(self.a*self.bound[0]))
+        cdf[x<self.bound[0]] = 0
+        cdf[x>self.bound[1]] = 1
+        return cdf
+
+    def inv_cdf(self,cdf):
+        factor = self.norm/(-self.a*np.log(10))
+        return (1/self.a)*torch.log10(10**(self.a*self.bound[0])-cdf/factor)
+
+
+def insert_jitters(spec,number,slope=-1.32,bound=[0.0,2]):
+    number = int(number)
+    location = torch.randint(len(spec), device=device,
+                             size=(1,number)).squeeze(0)
+
+    loglinear = LogLinearDistribution(slope,bound)
+    var = loglinear.inv_cdf(torch.rand(number,device=device))
+    amp = var**0.5
+
+    # half negative
+    half = torch.rand(number)>0.5
+    amp[half] = -amp[half]
+
+    # avoid inserting jitter to padded regions
+    mask = spec[location]>0
+    return location[mask],amp[mask]
+
+def jitter_redshift(batch, params, inst):
+    # original batch
+    spec, w, true_z = batch
+    wave_obs = inst.wave_obs
+
+    wave_mat = wave_obs*torch.ones_like(spec)
+    ncopy = len(params)
+
+    batch_out  = {}
+
+    data = []
+
+    begin = 0
+    # number of copys
+    for copy,param in enumerate(params):
+        batch_size = len(true_z)
+
+        #z_offset,n_lim = param
+        z_lim, n_lim = param
+
+        # uniform distribution
+        z_offset = z_lim*(2*torch.rand(batch_size,device=device)-1)
+
+        n_jit = np.random.randint(n_lim[0],n_lim[1],
+                                  size=batch_size)
+
+        z_new = true_z+z_offset
+
+        z_new[z_new<0] = 0 # avoid negative redshift
+        z_new[z_new>0.5] = 0.5 # avoid very high redshift
+
+        zfactor = ((1+z_new)/(1+true_z)).unsqueeze(1)*torch.ones_like(spec)
+
+        # redshift linear interpolation
+        spec_new = Interp1d()(wave_mat*zfactor,spec,wave_mat)
+        w_new = Interp1d()(wave_mat*zfactor,w,wave_mat)
+        w_new[w_new<=1e-6] = 0
+
+        record = []
+        for i in range(len(spec_new)):
+            loc,amp = insert_jitters(spec_new[i],n_jit[i])
+            spec_new[i][loc] += amp
+            w_new[i][loc] = 1/(amp**2+1/w_new[i][loc])
+            record.append([z_offset[i].item(),n_jit[i]])
+
+        med = spec_new.median(1,False).values[:,None]
+        med[med<1e-1] = 1e-1
+
+        spec_new /= med
+        #print("median:",spec_new.median(1,False).min())
+
+        if torch.isnan(spec_new).any():
+            nan_ind = torch.isnan(spec_new)
+            print("spec nan! z:", true_z[nan_ind],
+                  "offset:",z_offset[nan_ind])
+
+        end = begin+batch_size
+        data.append([spec_new,w_new,z_new])
+        batch_out[copy]={"param":record,"range":[begin,end]}
+        begin=end
+
+    new_batch = [torch.cat([d[i] for d in data]) for i in range(3)]
+    batch_out['batch'] = new_batch
+    return batch_out
