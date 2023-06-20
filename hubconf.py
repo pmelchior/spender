c5d7eb9554f51a629890185fe73730806ed5f4e4
Peter Melchior <peter.m.melchior@gmail.com>
2023-06-20 15:21:10 -0400

    hide imports

--- hubconf.py
+++ hubconf.py
@@ -1,13 +1,12 @@
 dependencies = ['torch', 'os']
 
-from spender import load_model
-from spender.model import SpectrumAutoencoder
-from spender.data.sdss import SDSS
+from spender import load_model as _load_model
+from spender.data.sdss import SDSS as _SDSS
 
 hub_server = "https://hub.pmelchior.net/"
 
 def sdss_I(device=None, **kwargs):
-    instrument = SDSS()
+    instrument = _SDSS()
     url = hub_server + "sdss.speculator+1.variable.lr_1e-3.latent_10.0.pt"
-    model = load_model(url, instrument, device=device)
+    model = _load_model(url, instrument, device=device)
     return instrument, model
