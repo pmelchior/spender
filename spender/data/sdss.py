4cedec764efd5d22e4f34f5f6613c3ca485e59f9
Peter Melchior <peter.m.melchior@gmail.com>
2026-09-22 20:25:32 -0400

    removed obsolete methods in SDSS

--- spender/data/sdss.py
+++ spender/data/sdss.py
@@ -1,4 +1,3 @@
-import glob
 import os
 import urllib.request
 
@@ -6,9 +5,8 @@ import astropy.io.fits as fits
 import astropy.table as aTable
 import numpy as np
 import torch
-import pickle
 from ..instrument import Instrument, get_skyline_mask
-from ..util import load_batch, interp1d
+from ..util import interp1d
 from .dataset import get_data_loader, get_features, write_dataset
 
 
@@ -22,6 +20,7 @@ class SDSS(Instrument):
     _wave_obs = 10 ** torch.arange(3.578, 3.97, 0.0001)
     _skyline_mask = get_skyline_mask(_wave_obs)
     _base_url = "https://data.sdss.org/sas/dr16/sdss/spectro/redux/26/spectra/lite/"
+    _id_columns = ["plate", "mjd", "fiberid"]
 
     def __init__(self, lsf=None, calibration=None):
         """Create instrument
@@ -34,8 +33,6 @@ class SDSS(Instrument):
             (optional) function to calibrate the observed spectrum
         """
         super().__init__(SDSS._wave_obs, lsf=lsf, calibration=calibration)
-
-    _id_columns = ["plate", "mjd", "fiberid"]
 
     @classmethod
     def get_data_loader(cls, path, which="train", batch_size=1024, shuffle=False, **kwargs):
@@ -86,135 +83,11 @@ class SDSS(Instrument):
             for start in range(0, len(fields), batch_size):
                 fields_ = fields[start : start + batch_size]
                 spec, w, z, norm, zerr = cls.make_batch(dir, fields_)
-                ids = np.array([[int(f[i]) for i in range(3)] for f in fields_])
-                yield dict(spec=spec, w=w, z=z, zerr=zerr, norm=norm, plate=ids[:, 0], mjd=ids[:, 1], fiberid=ids[:, 2])
+                ids = np.array([f[:3] for f in fields_], dtype=int)
+                yield dict(spec=spec, w=w, z=z, zerr=zerr, norm=norm, **dict(zip(cls._id_columns, ids.T)))
 
         features = get_features(len(cls._wave_obs), cls._id_columns)
         return write_dataset(path, batches(), features, cls._id_columns, **kwargs)
-
-    @classmethod
-    def convert_batches(cls, dir, path, tag="variable", **kwargs):
-        """Convert pickled batch files into a dataset
-
-        Parameters
-        ----------
-        dir: string
-            Root directory for data storage
-        path: string
-            Root directory of the dataset
-        tag: string
-            Name to specify which batch files to load
-        kwargs: dict
-            Additional arguments for :func:`spender.data.dataset.write_dataset`
-
-        Returns
-        -------
-        dict with the number of spectra in each split
-        """
-        def batches():
-            for filename in sorted(cls.list_batches(dir, tag=tag)):
-                spec, w, z, ids, norm, zerr = load_batch(filename)
-                ids = np.asarray(ids)
-                yield dict(spec=spec, w=w, z=z, zerr=zerr, norm=norm, plate=ids[:, 0], mjd=ids[:, 1], fiberid=ids[:, 2])
-
-        features = get_features(len(cls._wave_obs), cls._id_columns)
-        return write_dataset(path, batches(), features, cls._id_columns, **kwargs)
-
-    @classmethod
-    def list_batches(cls, dir, which=None, tag=None):
-        """List all batch files
-
-        Parameters
-        ----------
-        dir: string
-            Root directory for data storage
-        which: ['train', 'valid', 'test'] or None
-            Which subset of the spectra to return. If `None`, returns all of them.
-        tag: string
-            Name to specify which batch files to load
-
-        Returns
-        -------
-        list of filepaths
-        """
-        if tag is None:
-            tag = "variable"
-        classname = cls.__mro__[0].__name__
-        filename = f"{classname}{tag}_*.pkl"
-        batch_files = glob.glob(dir + "/" + filename)
-        batches = [item for item in batch_files if not "copy" in item]
-
-        NBATCH = len(batches)
-        train_batches = batches[: int(0.7 * NBATCH)]
-        valid_batches = batches[int(0.7 * NBATCH) : int(0.85 * NBATCH)]
-        test_batches = batches[int(0.85 * NBATCH) :]
-
-        if which == "test":
-            return test_batches
-        elif which == "valid":
-            return valid_batches
-        elif which == "train":
-            return train_batches
-        else:
-            return batches
-
-    @classmethod
-    def save_batch(cls, dir, batch, tag=None, counter=None):
-        """Save batch into a pickled file
-
-        Parameters
-        ----------
-        dir: string
-            Root directory for data storage
-        batch: `torch.tensor`, shape (N, L)
-            Spectrum batch
-        tag: string
-            Name to specify which batch file name
-        counter: int
-            Set to add a batch counter to the filename
-
-        Returns
-        -------
-        None
-
-        """
-        if tag is None:
-            tag = f"chunk{len(batch)}"
-        if counter is None:
-            counter = ""
-        classname = cls.__mro__[0].__name__
-        filename = os.path.join(dir, f"{classname}{tag}_{counter}.pkl")
-
-        with open(filename, "wb") as f:
-            pickle.dump(batch, f)
-
-    @classmethod
-    def save_in_batches(cls, dir, ids, tag=None, batch_size=1024):
-        """Save all spectra for given ids into batch files
-
-        Parameters
-        ----------
-        dir: string
-            Root directory for data storage
-        ids: list of (plate, mjd, fiberid)
-            Identifier of spectrum
-        tag: string
-            Name to specify the batch file name
-        batch_size: int
-            Number of spectra in each batch
-
-        Returns
-        -------
-        None
-
-        """
-        N = len(ids)
-        idx = np.arange(0, N, batch_size)
-        batches = np.array_split(ids, idx[1:])
-        for counter, ids_ in zip(idx, batches):
-            print(f"saving batch {counter} / {N}")
-            batch = cls.make_batch(dir, ids_)
-            cls.save_batch(dir, batch, tag=tag, counter=counter)
 
     @classmethod
     def get_spectrum(cls, dir, plate, mjd, fiberid, return_file=False):
